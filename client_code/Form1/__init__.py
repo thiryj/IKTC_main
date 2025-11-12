8ad457bd90f452c667ba76a031d658e16f14d6bd
john@thiry.com <john@thiry.com>
2025-11-12 18:04:21 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -316,7 +316,7 @@ class Form1(Form1Template):
       if trade_response and trade_response.get('order', {}).get('status') == 'ok':
         self.pending_order_id = trade_response['order']['id']
         self.label_trade_results_status.text = f"Order {self.pending_order_id} submitted. Awaiting fill..."
-        #self.label_trade_results_price.text = f"Limit Price: ${order_details['price']:.2f}"
+        self.label_trade_results_price.text = f"Limit Price: ${limit_price:.2f}"
         # Enable the timer toggle switch!
         self.checkbox_status_polling.enabled = True
         self.button_improve_price.enabled = True
