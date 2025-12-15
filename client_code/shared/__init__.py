9177437898a4f6e3d1023c2ad4348e3460ba01d5
john@thiry.com <john@thiry.com>
2025-12-15 17:13:32 +0000

    Edited shared

--- client_code/shared/__init__.py
+++ client_code/shared/__init__.py
@@ -2,12 +2,12 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-# This is a package.
+# This is a module.
 # You can define variables and functions here, and use them from any form. For example, in a top-level form:
 #
-#    from .. import Package1
+#    from ..shared import Module1
 #
-#    Package1.say_hello()
+#    Module1.say_hello()
 #
 
 
